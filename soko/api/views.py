b15299e11e39073ebfff945059de2130d1ebda89
James Njuguna <james.njuguna@tracom.co.ke>
2017-03-21 09:12:31 +0300

    updated the purchase cart api

--- soko/api/views.py
+++ soko/api/views.py
@@ -687,9 +687,6 @@ def purchase_cart():
                 total=cart.total,
                 quantity=cart.quantity
             )
-            db.session.add(purchase)
-            db.session.add(shopping_list)
-            db.session.add(delivery)
             order = Order(
                 user_id=user.id,
                 delivery_id=delivery.id,
@@ -700,6 +697,9 @@ def purchase_cart():
                 lng=data["lng"],
                 quantity=cart.quantity
             )
+            db.session.add(purchase)
+            db.session.add(shopping_list)
+            db.session.add(delivery)
             db.session.add(order)
             product = Product.query.get(cart.product_id)
             product.quantity = int(product.quantity) - int(purchase.quantity)
