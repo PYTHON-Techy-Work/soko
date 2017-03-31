4548c0c5c6140f98096868b00f1e53cdfa1303af
James Njuguna <james.njuguna@tracom.co.ke>
2017-03-31 11:09:44 +0300

    changed the lat and long into float so that they can be json serializable

--- soko/products/models.py
+++ soko/products/models.py
@@ -153,13 +153,13 @@ class Product(SurrogatePK, Model):
     def get_user_latitude(self):
         from soko.user.models import User
         user = User.query.filter_by(id=self.user_id).first()
-        lat = user.lat
+        lat = str(float(user.lat))
         return lat
 
     def get_user_longitude(self):
         from soko.user.models import User
         user = User.query.filter_by(id=self.user_id).first()
-        lng = user.lng
+        lng = str(float(user.lng))
         return lng
 
 
