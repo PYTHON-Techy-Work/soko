482e04286b76d8213bc4872c36e17e45c02ab846
James Njuguna <james.njuguna@tracom.co.ke>
2017-04-06 18:38:55 +0300

    changed the latitude and longitude from numeric to a string

--- soko/products/models.py
+++ soko/products/models.py
@@ -154,7 +154,7 @@ class Product(SurrogatePK, Model):
         from flask import jsonify
         from soko.user.models import User
         user = User.query.filter_by(id=self.user_id).first()
-        location ={"lat": float(user.lat), "lng": float(user.lng)}
+        location ={"lat": user.lat, "lng": user.lng}
         return jsonify(location)
 
 
